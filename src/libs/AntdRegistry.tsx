7309c57e5feb283273f9770b0e47115f034d00cc
Radosław Kłos <radoslaw@klos.dev>
2023-09-19 22:42:07 +0200

    Suppress some eslint errors

--- src/libs/AntdRegistry.tsx
+++ src/libs/AntdRegistry.tsx
@@ -7,8 +7,10 @@ import type Entity from '@ant-design/cssinjs/es/Cache';
 import { useServerInsertedHTML } from 'next/navigation';
 
 function StyledComponentsRegistry({ children }: { children: ReactNode }) {
+  // eslint-disable-next-line react-hooks/exhaustive-deps -- `createCache` is required to be called only once.
   const cache = useMemo<Entity>(() => createCache(), [ createCache ]);
   useServerInsertedHTML(() => (
+    // eslint-disable-next-line react/no-danger -- It's safe to use `dangerouslySetInnerHTML` here.
     <style id="antd" dangerouslySetInnerHTML={ { __html: extractStyle(cache, true) } } />
   ));
   return <StyleProvider cache={ cache }>{ children }</StyleProvider>;
