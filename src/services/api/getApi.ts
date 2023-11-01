ce114fcb632382c87d601c512ab03fa453d05d88
rklos <radoslaw@klos.dev>
2023-11-01 16:30:43 +0100

    Add request cache

--- src/services/api/getApi.ts
+++ src/services/api/getApi.ts
@@ -9,6 +9,12 @@ export function getApi(cookies?: string) {
     prefixUrl: `${host}/api`,
     timeout: 30 * 1000,
     credentials: 'include',
+    next: {
+      // Next throws a warning about mixing "cache" and "revalidate", but I don't use "cache",
+      // so I don't know why it's complaining. Maybe it's because Next 14?
+      // Anyway, it works, so I'm ignoring it.
+      revalidate: 3 * 60 * 60,
+    },
     headers: {
       Cookie: cookies,
     },
